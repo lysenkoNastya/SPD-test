169bbaea863d5ab66f0182a535ad36372fc1cb0a
Lysenko Nastya <Asya16828@gmail.com>
2018-05-23 03:21:03 +0300

    added tabs section

--- js/main.js
+++ js/main.js
@@ -19,11 +19,35 @@ $(document).ready(function(){
             }
         ]
     });
-
 });
 
 window.FontAwesomeConfig = {
     searchPseudoElements: true
 };
 
+var $wrapper = $('.section-tabs-wrapper'),
+    $allTabs = $wrapper.find('.tabs-content-list-item'),
+    $tabMenu = $wrapper.find('.tabs-menu-item');
+
+$allTabs.not(':first-of-type').hide();
+$tabMenu.filter(':first-of-type').find(':first').width('100%')
+
+$tabMenu.each(function(i) {
+    $(this).attr('data-tab', 'tab'+i);
+});
+
+$allTabs.each(function(i) {
+    $(this).attr('data-tab', 'tab'+i);
+});
+
+$tabMenu.on('click', function() {
+    var dataTab = $(this).data('tab'),
+        $getWrapper = $(this).closest($wrapper);
+
+    $getWrapper.find($tabMenu).removeClass('active');
+    $(this).addClass('active');
+
+    $getWrapper.find($allTabs).hide();
+    $getWrapper.find($allTabs).filter('[data-tab='+dataTab+']').show();
+});
 
