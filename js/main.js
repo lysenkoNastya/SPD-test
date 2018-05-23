745a68cca7a0223817daede206353ac0df1f4aaa
Lysenko Nastya <Asya16828@gmail.com>
2018-05-24 00:46:06 +0300

    responsived main menu and added parallax effect

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
+/*FontAwesome*/
+window.FontAwesomeConfig = {
+    searchPseudoElements: true
+};
+
 $(document).ready(function(){
+
+    /*Slick*/
     $('.autoplay').slick({
         dots: false,
         autoplaySpeed: 2000,
@@ -19,35 +26,45 @@ $(document).ready(function(){
             }
         ]
     });
+
+    /*Mobile navigation*/
+    $('.mainMenuBtn_js').click(function () {
+        $('body').toggleClass('main-nav-open');
+    });
+
+    $('.main-menu-arrow').click(function () {
+        $(this).closest('.main-menu-item').toggleClass('open');
+    });
+
+    /*Tabs*/
+    var $wrapper = $('.section-tabs-wrapper'),
+        $allTabs = $wrapper.find('.tabs-content-list-item'),
+        $tabMenu = $wrapper.find('.tabs-menu-item');
+
+    $allTabs.not(':first-of-type').hide();
+    $tabMenu.filter(':first-of-type').find(':first').width('100%')
+
+    $tabMenu.each(function(i) {
+        $(this).attr('data-tab', 'tab'+i);
+    });
+
+    $allTabs.each(function(i) {
+        $(this).attr('data-tab', 'tab'+i);
+    });
+
+    $tabMenu.on('click', function() {
+        var dataTab = $(this).data('tab'),
+            $getWrapper = $(this).closest($wrapper);
+
+        $getWrapper.find($tabMenu).removeClass('active');
+        $(this).addClass('active');
+
+        $getWrapper.find($allTabs).hide();
+        $getWrapper.find($allTabs).filter('[data-tab='+dataTab+']').show();
+    });
+
+
+
 });
 
-window.FontAwesomeConfig = {
-    searchPseudoElements: true
-};
-
-var $wrapper = $('.section-tabs-wrapper'),
-    $allTabs = $wrapper.find('.tabs-content-list-item'),
-    $tabMenu = $wrapper.find('.tabs-menu-item');
-
-$allTabs.not(':first-of-type').hide();
-$tabMenu.filter(':first-of-type').find(':first').width('100%')
-
-$tabMenu.each(function(i) {
-    $(this).attr('data-tab', 'tab'+i);
-});
-
-$allTabs.each(function(i) {
-    $(this).attr('data-tab', 'tab'+i);
-});
-
-$tabMenu.on('click', function() {
-    var dataTab = $(this).data('tab'),
-        $getWrapper = $(this).closest($wrapper);
-
-    $getWrapper.find($tabMenu).removeClass('active');
-    $(this).addClass('active');
-
-    $getWrapper.find($allTabs).hide();
-    $getWrapper.find($allTabs).filter('[data-tab='+dataTab+']').show();
-});
 
